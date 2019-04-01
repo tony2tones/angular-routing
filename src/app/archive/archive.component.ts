841c573a886b524d2b344b863d96037ff4d30d0a
Anthony Marques <anthony@armorica.gk>
2019-04-01 10:17:44 +0200

    added params snapshot to get month and year from the activeroute url

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'archive',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchiveComponent implements OnInit {
   title = "Archive Page"
-  constructor() { }
+  year: number;
+  month: number;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let params = this.route.snapshot.paramMap;
+    this.year = +params.get('year');
+    this.month = +params.get('month');
+
   }
 
 }
